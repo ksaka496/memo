a8c9f59269a4a109c458952aead321e36af20f7c
ks <k.saka496@gmail.com>
2022-05-29 17:08:14 +0900

    datetime edit

--- python/date.py
+++ python/date.py
@@ -1,11 +1,13 @@
 # datetime
 import datetime
 
+t_delta = datetime.timedelta(hours=9)
 JST = datetime.timezone(t_delta, 'JST')
 now = datetime.datetime.now(JST)
 
 print(repr(now))
 print(now)
 
+#YYYYMMDDHHMMSS
 d = now.strftime('%Y%m%d%H%M%S')
 print(d)
